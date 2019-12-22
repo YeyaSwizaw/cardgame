ae305c79619ffa0d1304530d5a25fbb9950c9fab
Samuel Sleight <samuel.sleight@gmail.com>
2019-12-22 17:36:22 +0000

    cargo fix --edition-idioms

--- src/client.rs
+++ src/client.rs
@@ -1,8 +1,4 @@
 #[macro_use] extern crate serde_derive;
-extern crate serde_json;
-extern crate serde;
-extern crate client;
-extern crate server;
 
 use std::{
     thread,
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,4 @@
 #[macro_use] extern crate serde_derive;
-extern crate serde_json;
-extern crate serde;
-extern crate server;
 
 use server::GameServer;
 use crate::game::TestGame;
